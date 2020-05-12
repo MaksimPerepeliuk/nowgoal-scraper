7cf890edf1476e78134d87f7ac879ec2e7a8b9e1
Max Perepelyuk <kostin7778@gmail.com>
2020-05-12 18:52:13 +0300

    setting vscode and test import

---
+++ nowg_parser/parser.py
@@ -0,0 +1,18 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+def get_html(url):
+    r = requests.get(url)
+    if r.ok:
+        return r.text
+    return print(r.status_code)
+
+
+def get_content(html):
+    soup = BeautifulSoup(html, 'lxml')
+    h1 = soup.find('a')
+    return h1
+
+
+print(get_content(get_html('https://www.google.com')))
