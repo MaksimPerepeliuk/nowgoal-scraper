176306a3e2db306f24be9b48a2d258afff4ee80b
MaksimPerepeliuk <maksim.perepelyuk@mail.ru>
2020-05-13 19:50:07 +0300

    create and extend competition urls file

--- nowg_parser/parser.py
+++ nowg_parser/parser.py
@@ -39,3 +39,22 @@ def get_page_data(champ_url):
             print(e)
             continue
     return highlight_urls(flatten(match_analyze_urls))
+
+
+def extend_urls_file():
+    doble_years = ['2018-2019', '2017-2018', '2016-2017',
+                   '2015-2016', '2014-2015', '2013-2014']
+    one_years = ['2019', '2018', '2017', '2016', '2015', '2014']
+    content = open('data/competitions_urls.txt').read()
+    urls = content.split(', ')
+    for url in [urls[0]]:
+        for doble_year, one_year in zip(doble_years, one_years):
+            if '2019-2020' in url:
+                new_urls = url.replace('2019-2020', doble_year)
+                write_text_file(new_urls, 'extend_comp_urls.txt')
+            elif '2020' in url:
+                new_urls = url.replace('2020', doble_year)
+                write_text_file(new_urls, 'extend_comp_urls.txt')
+
+
+extend_urls_file()
