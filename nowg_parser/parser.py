10e620dd0ec5aa132be49ca6f0fd452423fc3044
MaksimPerepeliuk <maksim.perepelyuk@mail.ru>
2020-05-13 13:17:21 +0300

    update chromedriver setting, and make get urls func

--- nowg_parser/parser.py
+++ nowg_parser/parser.py
@@ -1,18 +1,18 @@
-import requests
-from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import time
+
+url = 'http://info.nowgoal.group/en/League.aspx?SclassID=8'
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--window-size=1920x1080")
+chromedriver_path = '/home/max/projects/nowg_parser/nowg_parser/chromedriver'
+driver = webdriver.Chrome(options=chrome_options,
+                          executable_path=chromedriver_path)
 
 
-def get_html(url):
-    r = requests.get(url)
-    if r.ok:
-        return r.text
-    return print(r.status_code)
-
-
-def get_content(html):
-    soup = BeautifulSoup(html, 'lxml')
-    h1 = soup.find('a')
-    return h1
-
-
-print(get_content(get_html('http://www.google.com')))
+def get_match_analize_urls(champ_url, driver):
+    driver.get(champ_url)
+    time.sleep(0.5)
+    a = driver.find_elements_by_css_selector('a[title="Match Analyze"]')
+    return a
