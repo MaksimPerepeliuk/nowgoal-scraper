031f5282e818031171adead3307ede14b2d91f66
MaksimPerepeliuk <maksim.perepelyuk@mail.ru>
2020-05-13 22:37:02 +0300

    create a part which scrapping analize competitions urls

--- nowg_parser/parser.py
+++ nowg_parser/parser.py
@@ -10,51 +10,71 @@ driver = webdriver.Chrome(options=chrome_options,
                           executable_path=chromedriver_path)
 
 
-def flatten(lists):
-    return [elem for sublists in lists for elem in sublists]
-
-
 def write_text_file(data, file_name):
     with open(file_name, 'a') as file:
         file.write('{}, '.format(data))
 
 
+def flatten(lists):
+    return [elem for sublists in lists for elem in sublists]
+
+
 def highlight_urls(links):
+    if not links:
+        return
     for a in links:
-        write_text_file(a.get_attribute('href'), 'analyze_urls.txt')
+        write_text_file(a.get_attribute('href'), 'match_analize.txt')
 
 
-def get_page_data(champ_url):
+def write_analize_urls(champ_url):
     driver.get(champ_url)
-    time.sleep(2)
+    time.sleep(1)
+    cup_match_td = driver.find_elements_by_css_selector('td.cupmatch_rw2')
+    if cup_match_td:
+        cup_match_td[0].click()
+    time.sleep(1)
     tds = driver.find_elements_by_css_selector('td.lsm2')
-    match_analyze_urls = []
     for td in tds:
         try:
             td.click()
             links = driver.find_elements_by_css_selector(
                 'a[title="Match Analyze"]')
-            match_analyze_urls.append(links)
+            if not links:
+                links = driver.find_elements_by_css_selector(
+                    'a[title="Match Analysis"]')
+            highlight_urls(links)
         except Exception as e:
             print(e)
             continue
-    return highlight_urls(flatten(match_analyze_urls))
 
 
-def extend_urls_file():
+def get_extend_urls():
     doble_years = ['2018-2019', '2017-2018', '2016-2017',
                    '2015-2016', '2014-2015', '2013-2014']
     one_years = ['2019', '2018', '2017', '2016', '2015', '2014']
     content = open('data/competitions_urls.txt').read()
     urls = content.split(', ')
-    for url in [urls[0]]:
+
+    for url in urls:
         for doble_year, one_year in zip(doble_years, one_years):
             if '2019-2020' in url:
                 new_urls = url.replace('2019-2020', doble_year)
-                write_text_file(new_urls, 'extend_comp_urls.txt')
+                urls.append(new_urls)
             elif '2020' in url:
                 new_urls = url.replace('2020', doble_year)
-                write_text_file(new_urls, 'extend_comp_urls.txt')
+                urls.append(new_urls)
+    return urls
 
 
-extend_urls_file()
+def main():
+    urls = get_extend_urls()
+    for url in urls:
+        try:
+            write_analize_urls(url)
+        except Exception as e:
+            print(e)
+            continue
+
+
+if __name__ == '__main__':
+    main()
