2ae7b3dfa5d4edc7ae7ca1555fb7abfbabf39188
MaksimPerepeliuk <maksim.perepelyuk@mail.ru>
2020-05-14 20:26:21 +0300

    create function for getting previous match stat

--- nowg_parser/statistics_parser.py
+++ nowg_parser/statistics_parser.py
@@ -5,7 +5,6 @@ from bs4 import BeautifulSoup
 import requests
 import time
 
-
 # def get_page_source(url):
 #     chrome_options = Options()
 #     chrome_options.add_argument("--headless")
@@ -38,52 +37,56 @@ def find_weather_info(soup):
 
 
 def get_score(value):
-    print(value)
     if value == '':
         return 0
     return value.split('-')
 
 
-def get_head_to_head_stat(soup, home_team):
-    trs = soup.select('table#table_v3 tr')
-    home_away_score = {'goals': [0, 0], 'corner': [0, 0]}
-    away_home_score = {'goals': [0, 0], 'corner': [0, 0]}
-    count_hh_games = 0
+def data_cast(scorred, missed, count):
+    prefix = 'last{}m'.format(count)
+    return {prefix+'_score_goals': scorred['goals'],
+            prefix+'_score_corners': scorred['corners'],
+            prefix+'_missed_goals': missed['goals'],
+            prefix+'_missed_corners': missed['corners']}
 
+
+def get_stat(trs, team, type_):
+    scorred = {'goals': 0, 'corners': 0}
+    missed = {'goals': 0, 'corners': 0}
+    result_stat = {}
+    events_count = 0
     for tr in trs[4:]:  # возможно 3й элемент уже сыгран
         try:
             tds = tr.select('td')
-            hh_home_team = tds[2].text
+            table_home_team = tds[2].text
             final_score = get_score(tds[3].text)
             corner_score = get_score(tds[5].text)
-            if home_team == hh_home_team:
-                home_away_score['goals'][0] += int(final_score[0])
-                home_away_score['goals'][1] += int(final_score[1])
-                home_away_score['corner'][0] += int(corner_score[0])
-                home_away_score['corner'][1] += int(corner_score[1])
+            if team == table_home_team:
+                scorred['goals'] += int(final_score[0])
+                scorred['corners'] += int(corner_score[0])
+                missed['goals'] += int(final_score[1])
+                missed['corners'] += int(corner_score[1])
+                events_count += 1
             else:
-                away_home_score['goals'][0] += int(final_score[0])
-                away_home_score['goals'][1] += int(final_score[1])
-                away_home_score['corner'][0] += int(corner_score[0])
-                away_home_score['corner'][1] += int(corner_score[1])
+                scorred['goals'] += int(final_score[1])
+                scorred['corners'] += int(corner_score[1])
+                missed['goals'] += int(final_score[0])
+                missed['corners'] += int(corner_score[0])
+                events_count += 1
         except Exception:
             continue
-    home_team_goals = home_away_score['goals'][0] + away_home_score['goals'][1]
-    home_team_corners = home_away_score['corner'][0] + \
-        away_home_score['corner'][1]
-    away_team_goals = home_away_score['goals'][1] + away_home_score['goals'][0]
-    away_team_corners = home_away_score['corner'][1] + \
-        away_home_score['corner'][0]
 
-    return {'count_hh_games': count_hh_games,
-            'HH_total_home_goals': home_team_goals,
-            'HH_total_home_corners': home_team_corners,
-            'HH_total_away_goals': away_team_goals,
-            'HH_total_away_corners': away_team_corners,
-            'HH_curent_order_home_goals': home_away_score['goals'][0],
-            'HH_curent_order_away_goals': home_away_score['goals'][1],
-            'HH_curent_order_home_corners': home_away_score['corner'][0],
-            'HH_curent_order_away_corners': home_away_score['corner'][1]}
+        if events_count == 5:
+            data = data_cast(scorred, missed, 5)
+            result_stat.update(data)
+
+        if events_count == 10:
+            data = data_cast(scorred, missed, 10)
+            result_stat.update(data)
+
+    data = data_cast(scorred, missed, events_count)
+    result_stat.update(data)
+    return result_stat
 
 
 def get_main_stat(html):
@@ -98,8 +101,13 @@ def get_main_stat(html):
     total_score = sum(final_result)
     first_half_result = soup.find('span', title="Score 1st Half").text
     second_half_result = soup.find('span', title="Score 2nd Half").text
-    hh_stat = get_head_to_head_stat(soup, teams_title[0])
-    trs = soup.find_all('tr', bgcolor="#FFECEC")
+    h2h_stat = get_stat(
+        soup.select('table#table_v3 tr'), teams_title[0], 'head2head')
+    home_prev_stat = get_stat(
+        soup.select('table#table_v1 tr'), teams_title[0], 'home_prev')
+    away_prev_stat = get_stat(
+        soup.select('table#table_v2 tr'), teams_title[1], 'away_prev')
+    print(away_prev_stat)
     # print(second_half_result)
 
 
@@ -107,8 +115,48 @@ new_type = 'http://www.nowgoal.group/analysis/1763774.html'
 old_type = 'http://data.nowgoal.group/analysis/987471.html'
 from_new_to_old = 'http://data.nowgoal.group/analysis/1426148.html'
 
-get_main_stat(get_html(from_new_to_old))
+get_main_stat(get_html(old_type))
 # get_main_stat(get_html(old_type))
 
 # get_main_stat(get_html(new_type))
 # get_main_stat(get_html(old_type))
+
+
+# def get_stat(trs, team, type_):
+#     scorred = {'goals': 0, 'corners': 0}
+#     missed = {'goals': 0, 'corners': 0}
+#     for tr in trs[4:]:  # возможно 3й элемент уже сыгран
+#         try:
+#             tds = tr.select('td')
+#             hh_home_team = tds[2].text
+#             final_score = get_score(tds[3].text)
+#             corner_score = get_score(tds[5].text)
+#             print('hh - {} input - {}'.format(hh_home_team, team))
+#             if team == hh_home_team:
+#                 scorred['goal'] += int(final_score[0])
+#                 scorred['corners'] += int(corner_score[0])
+#                 missed['goal'] += int(final_score[1])
+#                 missed['corners'] += int(corner_score[1])
+#             else:
+#                 scorred['goal'] += int(final_score[1])
+#                 scorred['corners'] += int(corner_score[1])
+#                 missed['goal'] += int(final_score[0])
+#                 missed['corners'] += int(corner_score[0])
+#         except Exception:
+#             continue
+# home_team_goals = home_away_score['goals'][0] + away_home_score['goals'][1]
+# home_team_corners = home_away_score['corner'][0] + \
+#     away_home_score['corner'][1]
+# away_team_goals = home_away_score['goals'][1] + away_home_score['goals'][0]
+# away_team_corners = home_away_score['corner'][1] + \
+#     away_home_score['corner'][0]
+# print(home_away_score)
+# print(away_home_score)
+# return {type_+'_total_home_goals': home_team_goals,
+#         type_+'_total_home_corners': home_team_corners,
+#         type_+'_total_away_goals': away_team_goals,
+#         type_+'_total_away_corners': away_team_corners,
+#         type_+'_curent_order_home_goals': home_away_score['goals'][0],
+#         type_+'_curent_order_away_goals': home_away_score['goals'][1],
+#         type_+'_curent_order_home_corners': home_away_score['corner'][0],
+#         type_+'_curent_order_away_corners': home_away_score['corner'][1]}
