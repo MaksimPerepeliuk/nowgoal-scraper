45c3bb1b42c2c19d73c35b6b7fba042bc010a061
MaksimPerepeliuk <maksim.perepelyuk@mail.ru>
2020-05-17 16:48:48 +0300

    fix main functions and get ready to run parse match analize pages

--- nowg_parser/statistics_parser.py
+++ nowg_parser/statistics_parser.py
@@ -1,6 +1,8 @@
 from fake_useragent import UserAgent
 from bs4 import BeautifulSoup
 import requests
+import csv
+import traceback
 
 
 def get_html(url):
@@ -17,9 +19,12 @@ def strip_parentheses(string):
     return string
 
 
-def find_weather_info(soup):
+def get_weather_info(soup):
     tags = soup.find_all('div', class_='row')
-    return [tag.text for tag in tags if 'Weather' in tag.text][0]
+    weather_info = [tag.text for tag in tags if 'Weather' in tag.text]
+    if weather_info:
+        return weather_info[0].split('Weather:')[1]
+    return None
 
 
 def get_score(value):
@@ -34,6 +39,15 @@ def data_cast(scorred, missed, count):
             prefix+'_score_corners': scorred['corners'],
             prefix+'_missed_goals': missed['goals'],
             prefix+'_missed_corners': missed['corners']}
+
+
+def turn_into_num(str_):
+    if not str_:
+        return None
+    elif '.' in str_:
+        return float(str_)
+    else:
+        return int(str_)
 
 
 def get_score_missed_stat(trs, team, type_):
@@ -80,54 +94,17 @@ def get_common_stat(trs, team):
     for tr in trs[3:6]:
         tds = tr.select('td')
         title = tds[0].text
-        result_data[team+'_'+title+'_match'] = int(tds[1].text)
-        result_data[team+'_'+title+'_win'] = int(tds[2].text)
-        result_data[team+'_'+title+'_draw'] = int(tds[3].text)
-        result_data[team+'_'+title+'_lose'] = int(tds[4].text)
-        result_data[team+'_'+title+'_odds%'] = float(tds[5].text[:-1])
-        result_data[team+'_'+title+'_over'] = int(tds[7].text)
-        result_data[team+'_'+title+'_over%'] = float(tds[8].text[:-1])
-        result_data[team+'_'+title+'_under'] = int(tds[9].text)
-        result_data[team+'_'+title+'_under%'] = float(tds[10].text[:-1])
+        result_data[team+'_'+title+'_match'] = turn_into_num(tds[1].text)
+        result_data[team+'_'+title+'_win'] = turn_into_num(tds[2].text)
+        result_data[team+'_'+title+'_draw'] = turn_into_num(tds[3].text)
+        result_data[team+'_'+title+'_lose'] = turn_into_num(tds[4].text)
+        result_data[team+'_'+title+'_odds%'] = turn_into_num(tds[5].text[:-1])
+        result_data[team+'_'+title+'_over'] = turn_into_num(tds[7].text)
+        result_data[team+'_'+title+'_over%'] = turn_into_num(tds[8].text[:-1])
+        result_data[team+'_'+title+'_under'] = turn_into_num(tds[9].text)
+        result_data[team+'_'+title +
+                    '_under%'] = turn_into_num(tds[10].text[:-1])
     return result_data
-
-
-def get_stat(html):
-    soup = BeautifulSoup(html, 'lxml')
-    champ_title = strip_parentheses(
-        soup.find('span', class_='LName').find('a').text)
-    weather = find_weather_info(soup).split('Weather:')[1]
-    teams_titles = [
-        [a.text for a in soup.select('span.sclassName a')][0],
-        [a.text for a in soup.select('span.sclassName a')][1]]
-    final_result = [int(div.text)for div in soup.select('div.score')]
-    total_score = sum(final_result)
-    first_half_result = soup.find('span', title="Score 1st Half").text
-    second_half_result = soup.find('span', title="Score 2nd Half").text
-    data = {'champ_title': champ_title,
-            'weather': weather,
-            'home': teams_titles[0],
-            'away': teams_titles[1],
-            'result': '{}-{}'.format(final_result[0], final_result[1]),
-            'total_score': total_score,
-            'first_half': first_half_result,
-            'second_half': second_half_result}
-    h2h_stat = get_score_missed_stat(
-        soup.select('table#table_v3 tr'), teams_titles[0], 'head2head')
-    home_prev_stat = get_score_missed_stat(
-        soup.select('table#table_v1 tr'), teams_titles[0], 'home_prev')
-    away_prev_stat = get_score_missed_stat(
-        soup.select('table#table_v2 tr'), teams_titles[1], 'away_prev')
-    date_box = soup.select('tbody table tr td table.date_box tr')
-    home_box, away_box = date_box[:7], date_box[7:14]
-    home_common_stat = get_common_stat(home_box, 'home_team')
-    away_common_stat = get_common_stat(away_box, 'away_team')
-    data.update(h2h_stat)
-    data.update(home_prev_stat)
-    data.update(away_prev_stat)
-    data.update(home_common_stat)
-    data.update(away_common_stat)
-    return data
 
 
 def get_odds_info(html):
@@ -167,13 +144,79 @@ def get_odds_info(html):
                     data['close_OU_away'])[0]
             except Exception:
                 continue
-    print(data)
+    return data
 
 
-get_odds_info(get_html('http://data.nowgoal.com/oddscomp/987471.html'))
+def get_stat(html):
+    soup = BeautifulSoup(html, 'lxml')
+    main_url = 'http://data.nowgoal.group'
+    champ_title = strip_parentheses(
+        soup.find('span', class_='LName').find('a').text)
+    weather = get_weather_info(soup)
+    teams_titles = [
+        [a.text for a in soup.select('span.sclassName a')][0],
+        [a.text for a in soup.select('span.sclassName a')][1]]
+    final_result = [int(div.text)for div in soup.select('div.score')]
+    total_score = sum(final_result)
+    first_half_result = soup.find('span', title="Score 1st Half").text
+    second_half_result = soup.find('span', title="Score 2nd Half").text
+    data = {'champ_title': champ_title,
+            'weather': weather,
+            'home': teams_titles[0],
+            'away': teams_titles[1],
+            'result': '{}-{}'.format(final_result[0], final_result[1]),
+            'total_score': total_score,
+            'first_half': first_half_result,
+            'second_half': second_half_result}
+    h2h_stat = get_score_missed_stat(
+        soup.select('table#table_v3 tr'), teams_titles[0], 'head2head')
+    home_prev_stat = get_score_missed_stat(
+        soup.select('table#table_v1 tr'), teams_titles[0], 'home_prev')
+    away_prev_stat = get_score_missed_stat(
+        soup.select('table#table_v2 tr'), teams_titles[1], 'away_prev')
+    date_box = soup.select('tbody table tr td table.date_box tr')
+    home_box, away_box = date_box[:7], date_box[7:14]
+    home_common_stat = get_common_stat(home_box, 'home_team')
+    away_common_stat = get_common_stat(away_box, 'away_team')
+    odds_comp_url = soup.find('li', id='menu3').find('a')['href']
+    odds_info = get_odds_info(get_html(main_url+odds_comp_url))
+    data.update(h2h_stat)
+    data.update(home_prev_stat)
+    data.update(away_prev_stat)
+    data.update(home_common_stat)
+    data.update(away_common_stat)
+    data.update(odds_info)
+    return data
 
-# new_type = 'http://www.nowgoal.group/analysis/1763774.html'
-# old_type = 'http://data.nowgoal.group/analysis/987471.html'
-# from_new_to_old = 'http://data.nowgoal.group/analysis/1426148.html'
 
-# print(get_stat(get_html(old_type)))
+def write_csv(data, file_name, order):
+    with open(file_name, 'a') as file:
+        writer = csv.DictWriter(file, fieldnames=order)
+        writer.writerow(data)
+
+
+def unique(list_):
+    return list(set(list_))
+
+
+def get_old_type_urls(start, end):
+    new_type_urls = open('data/match_analize.txt').read()
+    old_type_urls = [url.replace('www', 'data')
+                     for url in new_type_urls.split(', ')]
+    unique_urls = unique(old_type_urls)
+    return unique_urls[start:end]
+
+
+def main(urls):
+    for i, url in enumerate(urls):
+        try:
+            data = get_stat(get_html(url))
+            order = list(data.keys())
+            write_csv(data, 'match_stat_0_1000.csv', order)
+        except Exception:
+            traceback.print_exc()
+            continue
+
+
+if __name__ == '__main__':
+    main(get_old_type_urls(0, 250))
---
+++ nowg_parser/thread3.py
@@ -0,0 +1,4 @@
+from nowg_parser.statistics_parser import main, get_old_type_urls
+
+
+main(get_old_type_urls(500, 750))
